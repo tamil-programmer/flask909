2ee855bee233545eba9e1815383829c25de10b29
suryaprogrammer <codewithsurya7@gmail.com>
2022-10-13 10:41:09 +0530

    first commit

---
+++ flask.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+import platform
+ 
+os= platform.uname()
+os = list(os)
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return  render_template("index.html",os=os[0],name=os[1],release=os[2],version=os[3],machine=os[4])
+
+
+if __name__ =="__main__":
+    app.run(debug=True,port = 7777)
+
+    
